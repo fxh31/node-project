b49025e91bbb2c4339c11d53de786c4c72871636
dawn <1194815021@qq.com>
2023-02-22 12:09:20 +0800

    完成使用sequelize连接MSQL数据库

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const express =  require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -19,14 +25,56 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', (req, res, next) => {
-//     console.log('总是要通过我');
-//     next();
-// })
+app.use((req, res, next) => { // 只有在传入请求的时候才会执行的中间件
+    User.findByPk(1)
+      .then(user => {
+        req.user = user; // req.user是sequelize保存在数据库中的数据
+        next();
+      })
+      .catch(err => console.log(err));
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {
+  constraints: true, // 开启定义关系约束
+  onDelete: 'CASCADE' // 级联删除
+});
+User.hasMany(Product);
 
-app.listen(3000);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, {
+  through: CartItem //这些连接存储的位置
+});
+Product.belongsToMany(Cart, { through: CartItem });
+
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
+sequelize
+  // .sync( { force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      User.create({ name: 'Max', email: 'test@test.com' })
+    }
+    return user;
+  })
+  .then(user => {
+    return user.createCart();
+  })
+  .then(cart => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
